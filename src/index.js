0258f06dd445fa392eb120367317ae98d27438ca
Oleh Shalyhin <cross14ek@yandex.ru>
2017-07-10 02:51:20 +0300

    added the input field for creating new list items, added items list

--- src/index.js
+++ src/index.js
@@ -2,3 +2,91 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+class CreateItemInput extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      itemName: '',
+    };
+
+    this.handleClick = this.handleClick.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleClick() {
+    this.props.onCreateClick(this.state.itemName);
+  }
+
+  handleChange(e) {
+    this.setState({
+      itemName: e.target.value,
+    });
+  }
+
+  render() {
+    return (
+        <div>
+          <button onClick={this.handleClick}>+</button>
+          <input type="text" value={this.state.itemName} onChange={this.handleChange} />
+        </div>
+    );
+  }
+}
+
+class ItemList extends React.Component {
+  render() {
+    const items = this.props.items.map((item, index) => (
+        <li key={index}>{item}</li>
+    ));
+    return (
+        <div>
+          <ul>
+            {items}
+          </ul>
+        </div>
+    );
+  }
+}
+
+class ProgressBar extends React.Component {
+  render() {
+    return (
+        <div></div>
+    );
+  }
+}
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      items: []
+    };
+
+    this.handleCreateClick = this.handleCreateClick.bind(this);
+  }
+
+  handleCreateClick(itemName) {
+    const items = this.state.items.slice();
+    items.push(itemName);
+    this.setState({
+      items: items
+    });
+  }
+
+  render() {
+    const items = this.state.items;
+    return (
+        <div>
+          <CreateItemInput onCreateClick={this.handleCreateClick} />
+          <ItemList items={items} />
+          <ProgressBar />
+        </div>
+    );
+  }
+}
+
+ReactDOM.render(
+    <App />,
+    document.getElementById("root")
+);
